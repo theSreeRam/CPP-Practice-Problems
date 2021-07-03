ea3241a22c332057de372e6e165897924245047c
Sreeram Panigrahi <panigrahi.sreeram@gmail.com>
2021-07-03 10:45:00 +0530

    Add insertion Sort

---
+++ insertionSort.cpp
@@ -0,0 +1,69 @@
+Insertion sort is a simple sorting algorithm that works similar to the way you sort playing cards in your hands. The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part.
+Algorithm 
+To sort an array of size n in ascending order: 
+1: Iterate from arr[1] to arr[n] over the array. 
+2: Compare the current element (key) to its predecessor. 
+3: If the key element is smaller than its predecessor, compare it to the elements before. Move the greater elements one position up to make space for the swapped element.
+
+// { Driver Code Starts
+// C program for insertion sort
+#include <stdio.h>
+#include <math.h>
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+ // } Driver Code Ends
+class Solution
+{   
+    public:
+    void insert(int arr[], int i)
+    {
+        //code here
+    }
+     public:
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        //Insertion Sort - we place the item in it's correct position
+        //At first, the first item is sorted , so we start from index = 1
+        for(int i=1; i<n; i++){
+            int temp = arr[i];
+            int j = i-1;
+            //we shift elements till they are larger than current item
+            while(j>=0 and arr[j]>temp){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = temp;
+        }
+    }
+};
+// { Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.insertionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+  // } Driver Code Ends
